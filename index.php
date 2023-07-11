871d0d6b704956761cd45fffb314dae835baba24
Ruma303 <matteoh8@gmail.com>
2023-07-11 19:03:25 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Live coding: <?php echo 'php-start'?></title>
+</head>
+<body>
+    <h1>HOME</h1>
+    <?php
+        echo "Siamo in PHP <br>"; // Commento single-line
+        EcHo "Hello World <br>";  #  Commento single-line
+        ECHO "Hello World";       /*
+                                    Commento multi-line
+                                  */
+        ?>
+    <h2></h2>
+</body>
+</html>
