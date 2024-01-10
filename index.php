a350fb9c8d3d9b24b45dc78df4cc3a89340325eb
Ruma303 <matteoh8@hotmail.it>
2024-01-10 15:42:56 +0100

    commit

--- index.php
+++ index.php
@@ -3,53 +3,42 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Live coding: <?php echo 'php-start'?></title>
+    <title>Live coding: <?php echo 'php-start'; ?></title>
 </head>
 <body>
     <h1>HOME</h1>
     <!-- //% Sintassi PHP -->
 
     <?php
-        echo "<h1>Siamo in PHP</h1>"; // Commento single-line
-        EcHo 'Hello World'  ;       #  Commento single-line
-        ECHO "Hello World ";       /*dffgfdgs
-                                    c'è una falla da sistemare
-                                  */
-        ?>
-    <!-- PHP o HTML -->
-    <h2><?= 'Hello World'; ?></h2>
-    <?= '<h2>Hello World</h2>'; ?>
-
-
-    <!-- Interruzione del codice -->
-    <?php /*
-    echo "<p>Codice eseguito</p>";
-    //exit("Codice interrotto da exit.");
-    die("Codice interrotto da die().");
-    echo "Codice non eseguito."; */
+        echo "<h1>Siamo in PHP</h1>";
+        EcHo 'Hello World<br>' ;
+        ECHO "Hello World ";
+        // Commento single-line
+        #  Commento single-line
+        /*
+        dffgfdgs
+        c'è una falla da sistemare
+        */
     ?>
 
 
+    <!-- PHP o HTML -->
+    <h2><?= 'Hello World asdfas'; ?></h2>
+    <?= '<h2>Hello World adasfd</h2>'; ?>
+
+
+    <!-- Interruzione del codice -->
+    <?php
+    echo "<p>Codice eseguito</p>";
+    exit;
+
+    die();
+    exit("Codice interrotto da exit.");
+
+    die("Codice interrotto da die().");
+
+    echo "Codice non eseguito.";
+    ?>
+
 </body>
 </html>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
